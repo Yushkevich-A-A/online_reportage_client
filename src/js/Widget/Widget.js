fd2ff91edd730dbbde168e74535715658ab7c9a2
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-08-03 20:35:08 +0300

    debug client

--- src/js/Widget/Widget.js
+++ src/js/Widget/Widget.js
@@ -1,3 +1,3 @@
 import CreateWidget from './CreateWidget/CreateWidget';
 
-const newWidget = new CreateWidget('https://yushkevich-online-news.herokuapp.com');
+const newWidget = new CreateWidget('http://192.168.1.57:7070');
